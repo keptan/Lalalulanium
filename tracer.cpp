d0610d9e881c0b903d24a2b7ff8361813de6259d
keptan <keptan@protonmail.com>
2019-10-13 16:10:15 -1000

    testing

--- tracer.cpp
+++ tracer.cpp
@@ -3,46 +3,11 @@
 #include "fun/sort.h"
 #include "fun/tree.h"
 #include "fun/range.h" 
+#include "tVector.h"
 #include <iostream>
 #include <algorithm>
 #include <math.h>
 
-
-
-template<typename T, size_t N>
-struct TVec
-{
-	template<size_t, class I>
-	using T0T_ = I;
-
-	template<class I, size_t... Is>
-	static auto tGen(std::index_sequence<Is...>) { return std::tuple<T0T_<Is, I>...>{}; }
-
-	template<class I, size_t IN>
-	static auto tGen() { return tGen<I>(std::make_index_sequence<IN>{}); }
-
-	using Tuple = decltype(tGen<T, N>());
-	Tuple test; 
-	/*
-	const vec& operator +(void) const {return *this;}
-	const vec  operator -(void) const 
-		{
-			std::array<float, length> out;
-
-			std::transform(e.begin(), e.end(), out.begin(), out.end(), 
-					[](const auto a){ return -a;});
-			return out;
-		}
-	float& operator [](int i) const {return e[i];}
-
-	vec& operator += (const vec& v2);
-	vec& operator -= (const vec& v2);
-	vec& operator *= (const vec& v2);
-	vec& operator /= (const vec& v2);
-	vec& operator *= (const c& v2);
-	vec& operator /= (const float t);
-	*/
-};
 
 auto outputPPM (void)
 {
@@ -97,11 +62,9 @@ auto imperativePPM (void)
 					const float g = distance(x, y, 0, width) / distance(height, 0, 0, width);
 					const float b = 1 - (distance(x, y, 0, width) / distance(height, 0, 0, width));
 
-					const int ir = int(255.99 * r);
-					const int ig = int(255.99 * g);
-					const int ib = int(255.99 * b);
+					const Color c (int(255.99 * r), int(255.99 * g), int(255.99 * b));
 
-					std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+					std::cout << c.r() << ' ' << c.g() << ' ' << c.b() << '\n';
 		}
 	}
 }
@@ -112,6 +75,12 @@ auto imperativePPM (void)
 int main (void)
 {
 //	imperativePPM();
-	outputPPM();
-	TVec<int, 3> {};
+//	outputPPM();
+	TVec<float, 3> t1 { std::make_tuple(1, 2, 3)};
+	TVec<float, 3> t2 { std::make_tuple(1, 5, 7)};
+	auto t3 = t1.cross(t2);
+
+	std::cout << std::get<0>(t1.res) << ' ' << std::get<0>(t3.res) << std::endl;
+	std::cout << t1.length() << std::endl;
+	
 }
