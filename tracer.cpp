58baae2cc50e5bf6e497a086c694c4ba1052b7bc
keptan <keptan@protonmail.com>
2019-10-13 16:58:13 -1000

    background and sphere collision test!

--- tracer.cpp
+++ tracer.cpp
@@ -43,6 +43,69 @@ auto outputPPM (void)
 	eval(rb);
 }
 
+
+
+bool sphereCollision (const Point& center, float radius, const Ray& r)
+{
+	Point oc = r.origin() - center; 
+	float a = r.direction().dot(r.direction()); 
+	float b = 2.0 * oc.dot(r.direction()); 
+	float c = oc.dot(oc) - radius * radius; 
+	float disc = b*b - 4*a*c;
+
+	return disc > 0; 
+}
+
+Color background (const Ray& r) 
+{
+	if( sphereCollision(Point(0, 0, -1), 0.5, r)) return Color(1, 0, 0);
+
+	Point unit_direction = r.direction().unitVector();
+	float t = 0.5 * (unit_direction.y() + 1.0); 
+	return (1.0 - t) * Color(1.0, 1.0, 1.0) + t*Color(0.5, 0.7, 1.0);
+}
+
+auto cameraTest (void)
+{
+	//image header, size and depth 
+	const int width = 200;
+	const int height  = 100;
+
+	std::cout << "P3\n" << width << ' ' << height << "\n255\n";
+	const Point lowerLeft (-2, -1, -1); 
+	const Point right		(4, 0, 0); 
+	const Point vertical  (0, 2, 0); 
+	const Point origin    (0, 0, 0);
+
+	//operate over pixel positions
+	const auto rb = Integers(0, height - 1) 
+					| Product(Integers(0, width - 1)) 
+					| Map([&](const auto tuple)
+					{
+					const auto distance =  [&](const auto x1, const auto y1, const auto x2, const auto y2) -> float
+					{
+							return std::sqrt(std::pow(y2 -y1, 2) + std::pow(x2 - x1, 2));
+					};
+						
+					const auto [x, y] = tuple;
+
+					const float v = x / float (height);
+					const float u = y / float (width);
+					const Ray r (origin, lowerLeft + u * right + v * vertical);
+					const Color c = background(r);
+
+
+					const int ir = int(255.99 * c.r());
+					const int ig = int(255.99 * c.g());
+					const int ib = int(255.99 * c.b());
+
+					std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+					});
+
+
+	eval(rb);
+}
+
 auto imperativePPM (void)
 {
 	const int width = 200;
@@ -76,11 +139,6 @@ int main (void)
 {
 //	imperativePPM();
 //	outputPPM();
-	TVec<float, 3> t1 { std::make_tuple(1, 2, 3)};
-	TVec<float, 3> t2 { std::make_tuple(1, 5, 7)};
-	auto t3 = t1.cross(t2);
+cameraTest();
 
-	std::cout << std::get<0>(t1.res) << ' ' << std::get<0>(t3.res) << std::endl;
-	std::cout << t1.length() << std::endl;
-	
 }
